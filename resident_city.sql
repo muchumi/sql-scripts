39f5b9a62c40e4b64388a551e02969f3aa9d7e3a
muchumi <wycliffemaina@ICT-HO.local>
2024-04-28 14:32:50 +0300

    Second scenario: Use of case to implement if-then logic;modified

--- resident_city.sql
+++ resident_city.sql
@@ -1,7 +1,7 @@
 create table residents (
     Id integer primary key,
-    StudentId integer not null unique,
-    StudentName text not null,
+    ResidentId integer not null unique,
+    ResidentName text not null,
     city text not null
 );
 insert into residents values(1, 101, 'Ammy White', 'New York');
@@ -9,7 +9,7 @@ insert into residents values(2, 102, 'Clark Kent', 'Smallville');
 insert into residents values(3, 103, 'Mark Lawson', 'Los Angeles');
 insert into residents values(4, 104, 'Lois Lane', 'Smallville');
 insert into residents values(5, 105, 'Lex Luthor', 'Smallville');
-select Id, StudentId, StudentName, City,
+select Id, ResidentId, ResidentName, City,
     case when City='Smallville' then 'Great Neighborhood'
          else 'Worst Neighborhood'
     end as ResidentsCityRating
