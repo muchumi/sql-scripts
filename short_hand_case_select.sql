2a41a333f62feec1dc3a529fd6b5033398b169f7
muchumi <muchumimaina94@gmail.com>
2024-04-29 08:34:16 +0300

    Shorthand CASE in SELECT

---
+++ short_hand_case_select.sql
@@ -0,0 +1,16 @@
+create table items(
+    Id integer primary key,
+    ItemId integer not null unique,
+    ItemName text not null,
+    Price float not null
+);
+insert into items values(1, 101, 'Samsung Bravia TV', 150000.00);
+insert into items values(2, 102, 'Hisense Android TV', 100000.00);
+insert into items values(3, 103, 'Vitron TV', 50000.00);
+insert into items values(4, 104, 'TCL TV', 35000.00);
+select Id, ItemId, ItemName, Price,
+    case Price when 150000 then 'Expensive'
+               when 35000.00 then 'Cheap'
+               else 'Affordable'
+    end as PriceRating
+from items
